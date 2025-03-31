6bfab838689d4e5b141d1d694fb107769196016c
Ylana Pereira <ylana.pereira2004@outlook.com>
2025-03-31 20:25:17 -0300

    Exercício 02
    Crie um programa que peça para o usuário digitar 2 números e o símbolo de uma operação matemática(prompt de
            novo). os possíveis ímbolos são: + , - , / , *
            Utilize a estrutura de decisão (if) para verificar qual símbolo a pessoa digitou, faça a operação escolhida com
            os 2 números e apresente o resultado com alert.
            Se a pessoa digitar um símbolo inválido mostre um alert escrito "operação inexistente"

---
+++ exercicio-02/ex02.js
@@ -0,0 +1,19 @@
+let num1 = prompt("Informe o primeiro número")
+let num2 = prompt("Informe o segundo número")
+let operador = prompt("Informe um operador matématico: +  -  / * ")
+
+if (operador == "+") {
+    let resultado = parseFloat(num1) + parseFloat(num2)
+    alert("O resultado da soma é: " + resultado)
+} else if (operador == "-") {
+    let resultado = parseFloat(num1) - parseFloat(num2)
+    alert("O resultado da subtração é: " + resultado)
+} else if (operador == "/") {
+    let resultado = parseFloat(num1) / parseFloat(num2)
+    alert("O resultado da divisão é: " + resultado)
+} else if (operador == "*") {
+    let resultado = parseFloat(num1) * parseFloat(num2)
+    alert("O resultado da multiplicação é: " + resultado)
+} else {
+    alert("Operação inexistente")
+}
